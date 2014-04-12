a134d508aff60ea3b50ab1ae37ae7ba277aca772
VenusArconada <venus.arconada@gmail.com>
2014-04-12 23:27:19 +0800

    Modified Circle.java with the correct codes

--- Circle.java
+++ Circle.java
@@ -1,21 +1,49 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Circle {
 	private static double cDiameter = 0;
-	private static double cPerimeter = 0;
+	private static double cCircumf = 0;
 	private static double cArea = 0;
+	private static Scanner inp;
+	private static double pi = 3.1416;
+	private String isCircumf = "circumference";
+	private String isCArea = "area";
+	
+	public static void main (String[] args) {
+	inp = new Scanner(System.in);
+	}
 	
 	public static void getCircumference() {
-	Scanner inp = new Scanner(System.in);
 	System.out.println("\nComputing for Circle's Circumference\n");
 	System.out.print("Enter circle's diameter: ");
 	
-	try {
-		cDiameter = inp.nextDouble();
-		cPerimeter = cDiameter*2;
-		}
+		if (isInputMatched(inp)) {
+			cCircumf = cDiameter*pi;
+			System.out.println("\nThe circle's circumference is =" + cCircumf);}
+		else
+			System.out.println("Invalid input. Exiting...");
 	}
-
+	
 	public static void getCircleArea() {
-	Scanner inp = new Scanner(System.in);
-
+		System.out.println("\nComputing for Circle's Area\n");
+		System.out.print("Enter circle's diameter: ");
+		
+		if(isInputMatched(inp)) {
+			cArea = pi*(Math.pow((cDiameter/2), 2));
+			System.out.println("\nThe circle's area is = " + cArea);}
+		else
+			System.out.println("Invalid input. Exiting...");
 	}
+	
+	private static boolean isInputMatched(Scanner i) {
+		try {
+			cDiameter = inp.nextDouble();
+			return true;	}
+		
+		catch(InputMismatchException nfe) {
+			inp.next();
+			return false;	
+		}
+	}	
 }
